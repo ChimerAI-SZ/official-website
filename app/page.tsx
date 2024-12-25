9e5a3ef24fab4572268887859f6cfd0717ff1562
lz <13714733197@163.com>
2024-12-25 17:06:38 +0800

    修改首页

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Carousel from "@components/Carousel"
+// import Carousel from "@components/Carousel"
 import ProductShowcase from "@components/ProductShowcase"
 import VisionMission from "@components/VisionMission"
 import WeServe from "@components/WeServe"
@@ -6,38 +6,41 @@ import WhyCreamoda from "@components/WhyCreamoda"
 import ShowcaseCarousel from "@components/ShowcaseCarousel"
 import OrderInfo from "@components/OrderInfo"
 
-const carouselImages = [
-  {
-    url: "/assets/images/home/swiper1.png",
-    alt: "Slide 1",
-    title: "INNOVATION"
-  },
-  {
-    url: "/assets/images/home/swiper2.png",
-    alt: "Slide 2",
-    title: "FREEDOM"
-  },
-  {
-    url: "/assets/images/home/swiper3.png",
-    alt: "Slide 3",
-    title: "INSPIRATION"
-  },
-  {
-    url: "/assets/images/home/swiper4.png",
-    alt: "Slide 4",
-    title: "EMOTION"
-  }
-]
+// const carouselImages = [
+//   {
+//     url: "/assets/images/home/swiper1.png",
+//     alt: "Slide 1",
+//     title: "INNOVATION"
+//   },
+//   {
+//     url: "/assets/images/home/swiper2.png",
+//     alt: "Slide 2",
+//     title: "FREEDOM"
+//   },
+//   {
+//     url: "/assets/images/home/swiper3.png",
+//     alt: "Slide 3",
+//     title: "INSPIRATION"
+//   },
+//   {
+//     url: "/assets/images/home/swiper4.png",
+//     alt: "Slide 4",
+//     title: "EMOTION"
+//   }
+// ]
 
 export default function Home() {
   return (
     <main className="pt-[70px] ">
-      <Carousel images={carouselImages} />
+      <ShowcaseCarousel />
+      {/* <ShowcaseCarousel />
+      <ShowcaseCarousel /> */}
+      {/* <Carousel images={carouselImages} /> */}
       <ProductShowcase />
       <VisionMission />
       <WeServe />
       <WhyCreamoda />
-      <ShowcaseCarousel />
+      {/* <ShowcaseCarousel /> */}
       <OrderInfo />
     </main>
   )
